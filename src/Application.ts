20cd62e75b11eb8e9eaec2d35b38f96d2e5de47d
Steve Sung <stevysung@gmail.com>
2024-04-24 22:41:18 +0900

    feat: Application으로 묶기

---
+++ src/Application.ts
@@ -0,0 +1,55 @@
+import { getInputElementValue, getRandomNumber } from "@/utils";
+import { RaceGame } from "@/RaceGame";
+import { HTMLRenderer } from "@/view/HTMLRenderer";
+
+export class Application {
+  private readonly startButtonSelector: string;
+  private readonly carCountSelector: string;
+  private readonly roundCountSelector: string;
+  private readonly roundResultSelector: string;
+
+  constructor({
+    startButtonSelector,
+    carCountSelector,
+    roundCountSelector,
+    roundResultSelector,
+  }: {
+    startButtonSelector: string;
+    carCountSelector: string;
+    roundCountSelector: string;
+    roundResultSelector: string;
+  }) {
+    this.startButtonSelector = startButtonSelector;
+    this.carCountSelector = carCountSelector;
+    this.roundCountSelector = roundCountSelector;
+    this.roundResultSelector = roundResultSelector;
+  }
+
+  init() {
+    const startBtn = document.querySelector(
+      this.startButtonSelector
+    ) as HTMLButtonElement;
+
+    startBtn.addEventListener("click", this.start.bind(this));
+  }
+
+  private start() {
+    const carCount = getInputElementValue(this.carCountSelector);
+    const roundCount = getInputElementValue(this.roundCountSelector);
+
+    const raceGame = new RaceGame({
+      carCount,
+      gameStrategy: () => getRandomNumber() >= 4,
+    });
+
+    const renderer = new HTMLRenderer({
+      selector: this.roundResultSelector,
+      roundCount,
+    });
+
+    raceGame.start({
+      roundCount,
+      renderer,
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-import { RaceGame } from "@/RaceGame";
-import { getInputElementValue, getRandomNumber } from "@/utils";
-import { HTMLRenderer } from "@/view/HTMLRenderer";
 import "./style.css";
+import { Application } from "@/Application";
 
-const startButton = document.querySelector("#startBtn") as HTMLButtonElement;
-
-startButton.addEventListener("click", () => {
-  const carCount = getInputElementValue("#carCount");
-  const roundCount = getInputElementValue("#roundCount");
-
-  const raceGame = new RaceGame({
-    carCount,
-    gameStrategy: () => getRandomNumber() >= 4,
-  });
-
-  const renderer = new HTMLRenderer({
-    selector: "#roundResult",
-    roundCount,
-  });
-
-  raceGame.start({
-    roundCount,
-    renderer,
-  });
+const application = new Application({
+  startButtonSelector: "#startBtn",
+  carCountSelector: "#carCount",
+  roundCountSelector: "#roundCount",
+  roundResultSelector: "#roundResult",
 });
+
+application.init();
