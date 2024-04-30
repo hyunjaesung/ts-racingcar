6b7aff43e20fc495a7aa3667b9acb408c9fb43b6
Hyun-Woo-Choi <coladrop98@gmail.com>
2024-04-30 16:53:55 +0900

    [remove] 초기화 테스트 삭제

--- test/car.test.ts
+++ test/car.test.ts
@@ -1,18 +1,6 @@
 import { describe, expect, it } from "vitest";
 import Car from "../src/components/car";
 
-describe("initialize Car class", () => {
-  it("Car class should have the given name in constructor, which is 'Car'", () => {
-    const carName = "Car";
-    const car = new Car(carName);
-    expect(car.name).toEqual(carName);
-  });
-  it("Car class should have 0 distance", () => {
-    const car = new Car("Car");
-    expect(car.distance).toEqual(0);
-  });
-});
-
 describe("move Car class", () => {
   it("moveForward should add 1 to the current distance", () => {
     const car = new Car("Car");
--- test/carRace.test.ts
+++ test/carRace.test.ts
@@ -1,35 +1,6 @@
 import { describe, expect, it } from "vitest";
 import CarRace from "../src/components/carRace";
 
-describe("initialize CarRace class", () => {
-  it("Car Race class hould have 0 carCount, 0 tryCount, and empty result at first", () => {
-    const carRace = new CarRace();
-    expect(carRace.carCount).toEqual(0);
-    expect(carRace.tryCount).toEqual(0);
-    expect(carRace.getResult().length).toEqual(0);
-  });
-
-  it("setCarCount function set carCount of Car Race class", () => {
-    const carRace = new CarRace();
-    const count1 = 1;
-    const count2 = 5;
-    carRace.carCount = count1;
-    expect(carRace.carCount).toEqual(count1);
-    carRace.carCount = count2;
-    expect(carRace.carCount).toEqual(count2);
-  });
-
-  it("setTryCount function set tryCount of Car Race class", () => {
-    const carRace = new CarRace();
-    const count1 = 1;
-    const count2 = 5;
-    carRace.tryCount = count1;
-    expect(carRace.tryCount).toEqual(count1);
-    carRace.tryCount = count2;
-    expect(carRace.tryCount).toEqual(count2);
-  });
-});
-
 describe("start function in Car Race Class", () => {
   it("the length of result should be equal to carCount", async () => {
     const carRace = new CarRace();
